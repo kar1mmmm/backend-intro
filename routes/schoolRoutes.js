fb5771fb6dd553454c88145d53a0f6e285f10202
kar1mmmm <abdulkariim317@gmail.com>
2026-07-09 22:39:09 +0700

    new route

--- routes/schoolRoutes.js
+++ routes/schoolRoutes.js
@@ -1,13 +1,23 @@
 const express = require('express');
 const router = express.Router();
-const schoolData = require('../data/schools.json');
+const fs = require('fs').promises;
+const path = require('path');
 
-router.get('/:jenjang', (req, res) => {
-    const jenjang = req.params.jenjang.toUpperCase();
-    if (schoolData[jenjang]) {
-        res.json({ status: "success", data: schoolData[jenjang] });
-    } else {
-        res.status(404).json({ status: "error", message: "Data tidak ditemukan" });
+router.get('/:jenjang', async (req, res) => {
+    try{
+        const jenjang = req.params.jenjang.toUpperCase();
+
+        const filePath = path.join(__dirname, '../data/schools.json'); 
+        const data = await fs.readFile(filePath, 'utf8');
+        const schoolData = JSON.parse(data);
+
+        if (schoolData[jenjang]){
+            res.json({status: "success", data: schoolData[jenjang]});
+        } else {
+            res.status(404).json({ status: "error", message: "Jenjang tidak tersedia."});
+        }
+    } catch (error){
+        res.status(500).json({status: "error", message: "Gagal memuat data."});
     }
 });
 
